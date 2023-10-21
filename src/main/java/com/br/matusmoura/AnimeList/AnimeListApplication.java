9f0d5afa78beecde36e10e381c6609504b6e4681
MatusMoura2 <matus.moura333x2@gmail.com>
2023-10-21 20:35:09 -0300

    first commit

---
+++ src/main/java/com/br/matusmoura/AnimeList/AnimeListApplication.java
@@ -0,0 +1,13 @@
+package com.br.matusmoura.AnimeList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/matusmoura/AnimeList/AnimeListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.matusmoura.AnimeList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
